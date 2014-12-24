42b9dc87fba994301a8c1484aea53a0d70cdd914
ajarac <angel@angel-TravelMate-B113>
2014-12-24 16:14:56 +0100

    arreglada la bases de datos y continuado con Android

--- sql/uTrolldb-data.sql
+++ sql/uTrolldb-data.sql
@@ -8,3 +8,12 @@ insert into user_roles values ('david', 'registered');
 
 insert into users values('angel', MD5('angel'), 'Angel', 'angel@mail.com', 22, 40, 40, 0);
 insert into user_roles values ('angel', 'registered');
+
+insert into users values('albert', MD5('albert'), 'Alberto', 'alberto@mail.com', 25, 40, 60, 0);
+insert into user_roles values ('albert', 'registered');
+
+insert into friend_list values(NULL, 'angel', 'david', 'accepted');
+
+insert into friend_list values(NULL, 'angel', 'albert', 'pending');
+
+insert into friend_list values(NULL, 'david', 'albert', 'accepted');
